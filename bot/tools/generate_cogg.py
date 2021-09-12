e7bba6bff22b20b5bb4ac27ef175625eb09f71dd
Rowan S-L <rowan.with.cats@gmail.com>
2021-09-12 11:46:58 -0400

    .cogg.bin files can now be autogenerated from normal coggs

--- bot/tools/generate_cogg.py
+++ bot/tools/generate_cogg.py
@@ -2,22 +2,34 @@ import pathlib
 import os, sys
 import dill
 import gzip
+import yaml
+import importlib
 
 cwd_path = pathlib.PurePath(pathlib.Path(__file__))
 main_path = str(cwd_path.parents[2])
+index_path = str(pathlib.PurePath(main_path, "bot", "modules", "index.yaml")) #yeet
 sys.path.append(main_path)
 
-#import thing to compress here
-from bot.modules.tag_cogg import TagCogg
+with open(index_path,'r') as f:
+    modules_index = list(yaml.safe_load_all(f))
+    modules = modules_index[0]['modules_gen']
 
-bitez = dill.dumps(TagCogg, byref=True)
-squashed_bitez = gzip.compress(bitez, 9)
-with pathlib.Path(main_path, "bot", "modules", "bin", "tag.cogg.bin").open("wb") as f:
-    f.write(squashed_bitez)
+imported = []
+if modules is not None:
+    for module in modules:
+        try:
+            imported.append(importlib.import_module(f"bot.modules.{module}_cogg"))
+        except ImportError as e:
+            print(f"failed to import {module} because of {e}")
+    print(f"imported {imported}")
 
-# with pathlib.Path(main_path, "bot", "modules", "bin", "builtin.cogg.bin").open("rb") as f:
-#     squashed_bitez = f.read()
-#     bitez = gzip.decompress(squashed_bitez)
-#     cogg = dill.loads(bitez)
-# instance = cogg()
-# instance.check()
+for module in imported:
+    for Cogg in module.module_coggs:
+        bitez = dill.dumps(Cogg, byref=True)
+        squashed_bitez = gzip.compress(bitez, 9)
+        # print(f'writing to {pathlib.PurePath(main_path, "bot", "modules", "bin", f"{Cogg.__name__.lower()[:-4]}.cogg.bin")}')
+        if not Cogg.__name__.lower().endswith("cogg"):
+            raise ValueError("class name must end with cogg")
+            break
+        with pathlib.Path(main_path, "bot", "modules", "bin", f"{Cogg.__name__.lower()[:-4]}.cogg.bin").open("wb") as f:
+            f.write(squashed_bitez)
